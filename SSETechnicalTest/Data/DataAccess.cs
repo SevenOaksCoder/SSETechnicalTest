0af291a6bc8168522fb3abf7a3e8e0a90a792f69
tonyfearn@yahoo.com <tonyfearn@yahoo.com>
2020-11-12 21:31:36 +0000

    Added Dapper, DataAccess and Models

---
+++ SSETechnicalTest/Data/DataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using SSETechnicalTest.Models;
+using System.Data.SqlClient;
+
+namespace SSETechnicalTest.Data
+{
+    public class DataAccess
+    {
+        // get all categories + mark features
+        public List<Category> GetCategories()
+        {
+            using (IDbConnection connect = new SqlConnection(Helper.ConnectionString("DBAccess")))
+            {
+                var categories = connect.Query<Category>("Category_GetCategories").ToList();
+                return categories;
+            }
+        }
+
+        // display a list of the selected category products
+        public List<Product> GetProductsByCategoryId(string categoryId)
+        {
+            using (IDbConnection connect = new SqlConnection(Helper.ConnectionString("DBAccess")))
+            {
+                var products =  connect.Query<Product>("Product_GetProductsByCategoryId @CategoryId", new { CategoryId = categoryId }).ToList();
+                return products;
+            }
+        }
+    }
+}
---
+++ SSETechnicalTest/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SSETechnicalTest
+{
+    public static class Helper
+    {
+        public static string ConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
